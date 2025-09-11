2ff1bebb4dd8ce14d61667ca966ecb34eb933427
Dev <nameofdeev@gmail.com>
2025-09-11 22:33:50 +0545

    Updated admin

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from newspaper.models import Post, Category, Tag
+from newspaper.models import Advertisement, Post, Category, Tag
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(Advertisement)
