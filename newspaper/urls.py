2c2f2346fa25764a00c24b4cf3f60eb6938ca82b
Dev <nameofdeev@gmail.com>
2025-09-13 17:15:01 +0545

    Separated articles in post_detail

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,4 +4,6 @@ from newspaper import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("post-list/", views.PostListView.as_view(), name="post-list"),
+    path("post-detail/<int:pk/", views.PostListView.as_view(), name="post-detail"),
+
 ]
