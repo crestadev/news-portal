dc7ba971c768b41e3adff7da7ced54e0a40d574d
Dev <nameofdeev@gmail.com>
2025-09-14 13:02:55 +0545

    Updated View of aboutview

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from newspaper.models import Advertisement, Post, Category, Tag
+from newspaper.models import Advertisement, OurTeam, Post, Category, Tag
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Advertisement)
+admin.site.register(OurTeam)
