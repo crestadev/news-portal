54ef56c5b4645e76d1eacaa1d75f73fb3cc957e4
Dev <nameofdeev@gmail.com>
2025-09-17 19:29:50 +0545

    Added app names accounts for proper planning and disstrubution  ahead

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "newspaper",  # your app
+    "newspaper",
+    "accounts"
 ]
 
 MIDDLEWARE = [
