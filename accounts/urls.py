aa87ac19b5ea3c646b6961495606b6e064cd4907
Dev <nameofdeev@gmail.com>
2025-09-18 10:09:04 +0545

    Added remplate of register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
@@ -8,4 +9,5 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(authentication_form=LoginForm),name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
