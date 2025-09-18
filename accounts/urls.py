682cd279e0c63299f66a4eb65cbbd10c04095683
Dev <nameofdeev@gmail.com>
2025-09-18 09:59:36 +0545

    Added logic of registration (views)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from accounts.forms import LoginForm
+
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(),name="login"),
+    path("login/", auth_views.LoginView.as_view(authentication_form=LoginForm),name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.views import View
+from .forms import RegistrationForm
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form= RegistrationForm()
+        return render(request, "registration/register.html", {"form": form})
+    
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            return redirect("accounts:login")
+        return render(request, "registration/register.html", {"form": form})
