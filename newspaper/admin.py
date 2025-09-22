6863b536bd26d416fb5e9ebd9ca4baf950344432
Dev <nameofdeev@gmail.com>
2025-09-22 17:07:34 +0545

    Updated admin.py

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 from newspaper.models import Advertisement, Comment, Contact, Newsletter, OurTeam, Post, Category, Tag
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
 admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Advertisement)
@@ -10,3 +11,10 @@ admin.site.register(OurTeam)
 admin.site.register(Contact)
 admin.site.register(Comment)
 admin.site.register(Newsletter)
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
+
+
+admin.site.register(Post, PostAdmin)
