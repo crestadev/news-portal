57373b3856b696bde8dbe21f8812572467ee43c7
Dev <nameofdeev@gmail.com>
2025-09-24 21:48:21 +0545

    Added Tag on serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,24 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
+from newspaper.models import Tag
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'username', 'email', 'groups', "first_name", "last_name"]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ['url', 'name']
+        fields = ['id', 'name']
+
+        
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
+        
