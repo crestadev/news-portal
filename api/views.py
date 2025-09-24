d8acbbdae0ef8bee44b0072e309f3086e7cdb0ee
Dev <nameofdeev@gmail.com>
2025-09-24 21:41:10 +0545

    Updated auth for admins

--- api/views.py
+++ api/views.py
@@ -10,13 +10,11 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
