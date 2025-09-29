627433367ee9ef4bcf30716cc12b354b703fe3a5
Dev <nameofdeev@gmail.com>
2025-09-29 21:36:44 +0545

    Updated admin panel

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,24 +1,52 @@
 from django.contrib import admin
-
-from newspaper.models import Advertisement, Comment, Contact, Newsletter, OurTeam, Post, Category, Tag
+from newspaper.models import (
+    Advertisement, Comment, Contact, Newsletter, OurTeam, Post, Category, Tag
+)
 from django_summernote.admin import SummernoteModelAdmin
 from unfold.admin import ModelAdmin
 
-admin.site.register(Tag)
-admin.site.register(Category)
-admin.site.register(Advertisement)
-admin.site.register(OurTeam)
-admin.site.register(Contact)
-admin.site.register(Comment)
-admin.site.register(Newsletter)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "icon")
+    search_fields = ("name",)
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    search_fields = ("title",)
 
+@admin.register(OurTeam)
+class OurTeamAdmin(admin.ModelAdmin):
+    list_display = ("name", "position")
+    search_fields = ("name", "position")
 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "created_at")
+    search_fields = ("name", "email", "subject")
 
-admin.register(Post)
-class CustomAdminClass(ModelAdmin, SummernoteModelAdmin):
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "user", "created_at")
+    search_fields = ("post__title", "user__username", "content")
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("email", "created_at")
+    search_fields = ("email",)
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin, SummernoteModelAdmin):
     summernote_fields = ("content",)
-
-    class Meta:
-        model = Post
+    list_display = (
+        "title", "author", "status", "category",
+        "published_at", "views_count", "is_breaking_news"
+    )
+    list_filter = ("status", "category", "tag", "is_breaking_news")
+    search_fields = ("title", "content")
+    ordering = ("-published_at",)
